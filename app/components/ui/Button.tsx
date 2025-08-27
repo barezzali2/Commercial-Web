0d2d7fbdb9933e509a9790cf8815ef8c130193f1
barezzali2 <barezrealy22@gmail.com>
2025-08-27 22:34:03 +0300

    reusable button

--- app/components/ui/Button.tsx
+++ app/components/ui/Button.tsx
@@ -1,10 +1,10 @@
 import React, { ReactNode } from 'react'
 
+
 type ButtonType = {
     children: ReactNode;
     className: string
 }
-
 
 function Button({ children, className }: ButtonType) {
   return (
