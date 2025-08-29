957c6bffa65f0b3aab6eb3812a734264558b4e76
barezzali2 <barezrealy22@gmail.com>
2025-08-29 03:40:56 +0300

    Header is good

--- app/components/layout/Header.tsx
+++ app/components/layout/Header.tsx
@@ -8,11 +8,10 @@ function Header() {
   return (
     <>
     <div className='bg-black text-white flex flex-row p-4 justify-center items-center'>
-        <p className=''>View the complete Kit</p>
-        <FaArrowRight className='ml-2'/>
+        <a href='#' className='flex items-center justify-center'>View the complete Kit <FaArrowRight className='ml-2'/></a>
     </div>
 
-    <header className='sticky top-0 bg-[#EAEEFE] z-50 overflow-hidden w-full'>
+    <header className='sticky top-0 bg-[#EAEEFE] z-50 overflow-hidden w-full md:px-5'>
         <div className='flex flex-row w-full flex-wrap items-center justify-between p-5'>
             <Logo />
             <Navbar />
