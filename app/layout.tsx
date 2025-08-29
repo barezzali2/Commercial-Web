cb68d92a3004478077feccc1bb6dbb0bee310d7b
barezzali2 <barezrealy22@gmail.com>
2025-08-29 03:39:53 +0300

    The booting process files

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "@/app/styles/globals.css";
 import Header from "@/app/components/layout/Header";
+import Footer from "@/app/components/layout/Footer";
 
 
 export const metadata: Metadata = {
@@ -16,6 +17,7 @@ export default function RootLayout({children,}: Readonly<{children: React.ReactN
           <main>
           {children}
           </main>
+        <Footer />
       </body>
     </html>
   );
