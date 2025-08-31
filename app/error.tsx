edc88bca4439108a4ed95b8080e9b16d8ad09eef
barezzali2 <barezrealy22@gmail.com>
2025-08-31 17:12:47 +0300

    Adding error boundry

--- app/error.tsx
+++ app/error.tsx
@@ -1,18 +1,22 @@
 "use client"
-import Link from 'next/link'
-import React from 'react'
 
-export default function error() {
+export default function Error({ error, reset } : {
+  error: Error & { digest?: string }
+  reset: () => void
+}) {
   return (
     <main className='flex justify-center items-center flex-col gap-6 m-20'>
       <h1 className='text-3xl font-semibold'>Something went wrong!</h1>
+      <p className='text-lg'>{error.message}</p>
 
-       <Link
-        href='/'
-        className='inline-block bg-indigo-700 text-white px-6 py-3 text-lg rounded-2xl'
+       <button
+        onClick={
+          () => reset()
+        }
+        className='inline-block bg-indigo-700 text-white px-6 py-3 text-lg rounded-2xl cursor-pointer'
       >
         Try again
-      </Link>
+      </button>
     </main>
   )
 }
