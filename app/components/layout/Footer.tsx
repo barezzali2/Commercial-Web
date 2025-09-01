0e56aa78c475964d741722371d5cd6c423266266
barezzali2 <barezrealy22@gmail.com>
2025-09-01 11:27:08 +0300

    Fixing some issue in the footer

--- app/components/layout/Footer.tsx
+++ app/components/layout/Footer.tsx
@@ -39,7 +39,9 @@ export default function Footer() {
 
       {/* Logo and social media icon stuff */}
       <div>
-        <Logo />
+        <div className='inline-block'>
+          <Logo />
+        </div>
         <p className='max-w-80 font-normal text-balance text-[#BCBCBC] mt-10 md:text-[15px]'>Effortlessly turn your ideas into a fully functional, responsive, no-code SaaS website.</p>
         <div className='flex items-center gap-5 mt-10 md:mt-18'>
           {socialIcons.map((icon: SocialType, i: number) => (
@@ -51,38 +53,38 @@ export default function Footer() {
 
       {/* Links */}
       <div className='text-[#7B7B7B] mt-15 md:grid md:grid-cols-4 md:mt-0 md:gap-12 lg:gap-18'>
-        <div className='flex flex-col gap-4 text-sm'>
+        <div className='flex flex-col gap-4 text-sm [&_a]:w-fit'>
           <h3 className='text-white'>Product</h3>
-          <Link href="#">Features</Link>
-          <Link href="#">Integrations</Link>
-          <Link href="#">Updates</Link>
-          <Link href="#">FAQ</Link>
+          <Link href="#" className='lg:hover:text-[#bababa]'>Features</Link>
+          <Link href="#" className='lg:hover:text-[#bababa]'>Integrations</Link>
+          <Link href="#" className='lg:hover:text-[#bababa]'>Updates</Link>
+          <Link href="#" className='lg:hover:text-[#bababa]'>FAQ</Link>
           <Link href="#">Pricing</Link>
         </div>
 
-        <div className='flex flex-col gap-4 mt-10 md:mt-0 text-sm'>
+        <div className='flex flex-col gap-4 mt-10 md:mt-0 text-sm [&_a]:w-fit'>
           <h3 className='text-white'>Company</h3>
-          <Link href="#">About</Link>
-          <Link href="#">Blog</Link>
-          <Link href="#">Careers</Link>
-          <Link href="#">Manifesto</Link>
-          <Link href="#">Press</Link>
-          <Link href="#">Contact</Link>
+          <Link href="#" className='lg:hover:text-[#bababa]'>About</Link>
+          <Link href="#" className='lg:hover:text-[#bababa]'>Blog</Link>
+          <Link href="#" className='lg:hover:text-[#bababa]'>Careers</Link>
+          <Link href="#" className='lg:hover:text-[#bababa]'>Manifesto</Link>
+          <Link href="#" className='lg:hover:text-[#bababa]'>Press</Link>
+          <Link href="#" className='lg:hover:text-[#bababa]'>Contact</Link>
         </div>
 
-        <div className='flex flex-col gap-4 mt-10 md:mt-0 text-sm'>
+        <div className='flex flex-col gap-4 mt-10 md:mt-0 text-sm [&_a]:w-fit'>
           <h3 className='text-white'>Resources</h3>
-          <Link href="#">Examples</Link>
-          <Link href="#">Community</Link>
-          <Link href="#">Guides</Link>
-          <Link href="#">Docs</Link>
+          <Link href="#" className='lg:hover:text-[#bababa]'>Examples</Link>
+          <Link href="#" className='lg:hover:text-[#bababa]'>Community</Link>
+          <Link href="#" className='lg:hover:text-[#bababa]'>Guides</Link>
+          <Link href="#" className='lg:hover:text-[#bababa]'>Docs</Link>
         </div>
 
-        <div className='flex flex-col gap-4 mt-10 md:mt-0 text-sm'>
+        <div className='flex flex-col gap-4 mt-10 md:mt-0 text-sm [&_a]:w-fit'>
           <h3 className='text-white'>Legal</h3>
-          <Link href="#">Privacy</Link>
-          <Link href="#">Terms</Link>
-          <Link href="#">Security</Link>
+          <Link href="#" className='lg:hover:text-[#bababa]'>Privacy</Link>
+          <Link href="#" className='lg:hover:text-[#bababa]'>Terms</Link>
+          <Link href="#" className='lg:hover:text-[#bababa]'>Security</Link>
         </div>
       </div>
 
